f7f9f6180bef28b88f0ec2b4426f3892ac8b5bb4
Cephelo <79186646+Cephelo@users.noreply.github.com>
2024-07-25 01:16:55 -0500

    Update LunarConfig.java

--- forge/src/main/java/com/mrbysco/lunar/config/LunarConfig.java
+++ forge/src/main/java/com/mrbysco/lunar/config/LunarConfig.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class LunarConfig {
 	public static class Common {
 		public final DoubleValue lunarEventChance;
+		public final BooleanValue lunarEventWeatherCleanse;
 
 		public final IntValue bloodMoonWeight;
 		public final IntValue crimsonMoonWeight;
@@ -32,6 +33,10 @@ public class LunarConfig {
 			lunarEventChance = builder
 					.comment("Chance of a Lunar Event happening when night falls [Default: 0.4]")
 					.defineInRange("lunarEventChance", 0.4, 0, 1);
+			
+			lunarEventWeatherCleanse = builder
+					.comment("Allows for rain to cleanse the effects of a Lunar Event [Default: true]")
+					.define("lunarEventWeatherCleanse", true);
 
 			builder.pop();
 			builder.comment("Event settings")
