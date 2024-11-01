a520ae4276762f50003dfe813bf9a59ec1915c06
anangryraven <39865912+AnAngryRaven@users.noreply.github.com>
2024-11-01 08:40:27 -0400

    [Forge] Add config options
    Tested both phases and days for completion's sake, and appears to work (as was expected)

--- forge/src/main/java/com/mrbysco/lunar/config/LunarConfig.java
+++ forge/src/main/java/com/mrbysco/lunar/config/LunarConfig.java
@@ -22,6 +22,25 @@ public class LunarConfig {
 		public final IntValue badOmenMoonWeight;
 		public final IntValue heroMoonWeight;
 		public final IntValue eclipseMoonWeight;
+		public final IntValue bloodMoonPhase;
+		public final IntValue crimsonMoonPhase;
+		public final IntValue minerMoonPhase;
+		public final IntValue whiteMoonPhase;
+		public final IntValue bigMoonPhase;
+		public final IntValue tinyMoonPhase;
+		public final IntValue badOmenMoonPhase;
+		public final IntValue heroMoonPhase;
+		public final IntValue eclipseMoonPhase;
+		public final IntValue bloodMoonDay;
+		public final IntValue crimsonMoonDay;
+		public final IntValue minerMoonDay;
+		public final IntValue whiteMoonDay;
+		public final IntValue bigMoonDay;
+		public final IntValue tinyMoonDay;
+		public final IntValue badOmenMoonDay;
+		public final IntValue heroMoonDay;
+		public final IntValue eclipseMoonDay;
+
 
 		public final ConfigValue<List<? extends String>> crimsonReplacements;
 
@@ -72,6 +91,86 @@ public class LunarConfig {
 			eclipseMoonWeight = builder
 					.comment("Weight of the Eclipse Moon Event [Default: 2]")
 					.defineInRange("eclipseMoonWeight", 2, 0, 100);
+
+			builder.pop();
+			builder.comment("Moon phase settings. -1 is disabled, 0-7 are moon phases, starting with a full moon at 0.")
+					.push("Phases");
+
+			bloodMoonPhase = builder
+					.comment("Moon phase that the Blood Moon should trigger on. [Default: -1]")
+					.defineInRange("bloodMoonPhase", -1, -1, 7);
+
+			crimsonMoonPhase = builder
+					.comment("Moon phase that the Crimson Moon should trigger on. [Default: -1]")
+					.defineInRange("crimsonMoonPhase", -1, -1, 7);
+
+			minerMoonPhase = builder
+					.comment("Moon phase that the Miner Moon should trigger on. [Default: -1]")
+					.defineInRange("minerMoonPhase", -1, -1, 7);
+
+			whiteMoonPhase = builder
+					.comment("Moon phase that the White Moon should trigger on. [Default: -1]")
+					.defineInRange("whiteMoonPhase", -1, -1, 7);
+
+			bigMoonPhase = builder
+					.comment("Moon phase that the Big Moon should trigger on. [Default: -1]")
+					.defineInRange("bigMoonPhase", -1, -1, 7);
+
+			tinyMoonPhase = builder
+					.comment("Moon phase that the Tiny Moon should trigger on. [Default: -1]")
+					.defineInRange("tinyMoonPhase", -1, -1, 7);
+
+			badOmenMoonPhase = builder
+					.comment("Moon phase that the Bad Omen Moon should trigger on. [Default: -1]")
+					.defineInRange("badOmenMoonPhase", -1, -1, 7);
+
+			heroMoonPhase = builder
+					.comment("Moon phase that the Hero Moon should trigger on. [Default: -1]")
+					.defineInRange("heroMoonPhase", -1, -1, 7);
+
+			eclipseMoonPhase = builder
+					.comment("Moon phase that the Eclipse Moon should trigger on. [Default: -1]")
+					.defineInRange("eclipseMoonPhase", -1, -1, 7);
+
+			builder.pop();
+			builder.comment("Lunar event day settings. 0 is disabled, anything above is enabled.")
+					.push("Days");
+
+			bloodMoonDay = builder
+					.comment("Triggers the Blood Moon every number of days, as defined. [Default: 0]")
+					.defineInRange("bloodMoonDay", 0, 0, 255);
+
+			crimsonMoonDay = builder
+					.comment("Triggers the Crimson Moon every number of days, as defined. [Default: 0]")
+					.defineInRange("crimsonMoonDay", 0, 0, 255);
+
+			minerMoonDay = builder
+					.comment("Triggers the Miner Moon every number of days, as defined. [Default: 0]")
+					.defineInRange("minerMoonDay", 0, 0, 255);
+
+			whiteMoonDay = builder
+					.comment("Triggers the White Moon every number of days, as defined. [Default: 0]")
+					.defineInRange("whiteMoonDay", 0, 0, 255);
+
+			bigMoonDay = builder
+					.comment("Triggers the Big Moon every number of days, as defined. [Default: 0]")
+					.defineInRange("bigMoonDay", 0, 0, 255);
+
+			tinyMoonDay = builder
+					.comment("Triggers the Tiny Moon every number of days, as defined. [Default: 0]")
+					.defineInRange("tinyMoonDay", 0, 0, 255);
+
+			badOmenMoonDay = builder
+					.comment("Triggers the Bad Omen Moon every number of days, as defined. [Default: 0]")
+					.defineInRange("badOmenMoonDay", 0, 0, 255);
+
+			heroMoonDay = builder
+					.comment("Triggers the Hero Moon every number of days, as defined. [Default: 0]")
+					.defineInRange("heroMoonDay", 0, 0, 255);
+
+			eclipseMoonDay = builder
+					.comment("Triggers the Eclipse Moon every number of days, as defined. [Default: 0]")
+					.defineInRange("eclipseMoonDay", 0, 0, 255);
 
 			builder.pop();
 			builder.comment("Crimson Replacement settings")
